49d6a66e41098ce2f8e1766a68d547f167e4a87a
“Pedro <pedrohenriquesampaiodenovaes@gmail.com>
2022-04-19 19:16:36 -0300

    Adicionado atributos nas tags que melhoram a acessibilidade para botão de menu mobile

--- src/js/modules/menu-mobile.js
+++ src/js/modules/menu-mobile.js
@@ -7,11 +7,18 @@ export default function initMobileMenu() {
       if(event.type === 'touchstart') event.preventDefault();
       btnMenu.classList.toggle('ativo');
       menuItems.classList.toggle('ativo');
+
+      const ativo = btnMenu.classList.contains('ativo')
+      event.currentTarget.setAttribute('aria-expanded', ativo)
+      if (ativo) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+      } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+      }
     }
 
     ['click', 'touchstart'].forEach(eventUser => {
       btnMenu.addEventListener(eventUser, toggleMenu);
     })
   }
-  
 }
